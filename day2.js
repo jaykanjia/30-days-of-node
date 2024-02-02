ea14b63f0c006cca8ccbedcb90375fb215bef000
Jay Kanjia <jaykanjia11@gmail.com>
2024-02-02 23:09:32 +0530

    scaler day2 task

--- day2.js
+++ day2.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 function writeToFile(filePath, content) {
 	fs.writeFile(filePath, content, (err) => {
 		if (err) {
-			return console.log(`Error writing to file: ${err}`);
+			return console.log(`Error writing to file: ${err.message}`);
 		}
 		return console.log(`Data written to ${filePath}`);
 	});
